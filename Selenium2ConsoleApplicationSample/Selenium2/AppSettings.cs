14dc6bf4f2b66d9939f37c614c0e727d74670a25
Wataru Hoshino <wataru-hoshino@gmo.jp>
2016-09-29 16:29:41 +0900

    Search on Google using the Selenium2

---
+++ Selenium2ConsoleApplicationSample/Selenium2/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Selenium2ConsoleApplicationSample.Selenium2
+{
+    internal static class AppSettings
+    {
+        /// <summary>
+        /// ブラウザ名
+        /// </summary>
+        public enum BrowserName
+        {
+            None,
+            Chrome,
+            Firefox,
+            InternetExplorer,
+            Edge,
+            Safari
+        }
+    }
+}
